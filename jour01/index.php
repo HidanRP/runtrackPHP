eb1dac0d23efc6a4efd11f3c87630beec7e548ca
HidanRP <maxime.cuadro@laplateforme.io>
2025-01-14 13:31:08 +0100

    💄Job 1.2 ainsi que le 1.3

--- jour01/index.php
+++ jour01/index.php
@@ -1,9 +1,18 @@
 <?php echo "Hello La Plateforme" ?>
 
-<?php $str="La Plateforme";
-      $str2="Vive";
-      $str3="!" ?>
+<?php $str="La Plateforme" ;
+      $str2="Vive" ;
+      $str3="!" ;
+
+      echo "$str2 $str $str3"
+?>
 
 
+<?php $val=6 ;
+      echo $val + 4
+?>
 
 
+<?php $myBool=false ;
+
+      echo "$myBool" ?>
